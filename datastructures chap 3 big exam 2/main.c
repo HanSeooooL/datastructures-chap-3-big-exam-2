1b6e4f3d9831f42422746db93b86d6e5f4da7017
HANSEOL <nothinonthere@icoud.com>
2023-04-12 11:04:44 +0900

    Initial Commit

---
+++ datastructures chap 3 big exam 2/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  datastructures chap 3 big exam 2
+//
+//  Created by 한설 on 2023/04/12.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
